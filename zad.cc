68b4de6223a3837ae00624e7b19f78523fe40150
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-05-06 23:12:43 +0200

    comments

--- zad.cc
+++ zad.cc
@@ -18,8 +18,6 @@ NS_LOG_COMPONENT_DEFINE("WifiSimpleAdhocGrid");
 #define GRID_DISTANCE 200
 #define PACKET_SIZE 200
 
-
-
 Gnuplot2dDataset data[25];
 /// Trace function for remaining energy at node.
 
@@ -61,7 +59,7 @@ Vector randomManPosition(int x, int y, Vector m_position) {
 void SensorRxCallback(Ptr<Socket> sendSocket, Ptr<Socket> socket) {
     //    NS_LOG_UNCOND("Sensor received -> Sending");
     while (socket->Recv()) {
-        sendSocket->Send(Create<Packet> (PACKET_SIZE));
+        sendSocket->Send(Create<Packet>(PACKET_SIZE));
     }
 }
 
@@ -75,16 +73,14 @@ void FarmerRxCallback(Ptr<Socket> socket) {
         //        NS_LOG_UNCOND("FARMER RECEIVED");
         std::cout << ".";
     }
-
 }
 
 static void farmerSend(Ptr<Socket> socket[]) {
     for (int i = 0; i < 25; i++) {
         //        NS_LOG_UNCOND("FARMER SEND");
-        socket[i]->Send(Create<Packet> (PACKET_SIZE));
+        socket[i]->Send(Create<Packet>(PACKET_SIZE));
     }
     std::cout << "\nBC\n";
-
 }
 
 static void ManWalking(Ptr<ConstantPositionMobilityModel> cvMob, Ptr<Socket> socket[]) {
@@ -93,7 +89,6 @@ static void ManWalking(Ptr<ConstantPositionMobilityModel> cvMob, Ptr<Socket> soc
     //    NS_LOG_UNCOND("FARMER MOVED");
     Simulator::Schedule(Seconds(30.0), &farmerSend, socket);
     Simulator::Schedule(Seconds(35.0), &ManWalking, cvMob, socket);
-
 }
 
 int main(int argc, char *argv[]) {
@@ -103,27 +98,11 @@ int main(int argc, char *argv[]) {
     graf.SetTitle("Baterie");
     graf.SetLegend("Cas[s]", "Ostavajuca Energia v Baterii[J]");
 
-
-
     for (int i = 0; i < 25; i++) {
         data[i].SetTitle("node " + std::to_string(i));
         data[i].SetStyle(Gnuplot2dDataset::LINES);
     }
 
-
-
-
-    Gnuplot graf2("QoS.svg");
-    graf2.SetTerminal("svg");
-    graf2.SetTitle("QoS");
-    graf2.SetLegend("Vzdialenost [m]", "Priepustnost[Mbit/s]");
-
-    //graf.AppendExtra("set xrange[20:100]");
-
-    Gnuplot2dDataset data2;
-    data2.SetTitle("strata udajov");
-    data2.SetStyle(Gnuplot2dDataset::POINTS);
-    //data.SetErrorBars(Gnuplot2dDataset::Y);
 
     uint32_t numNodes = 25; // by default, 5x5
 
@@ -138,7 +117,6 @@ int main(int argc, char *argv[]) {
 
     NodeContainer f;
     f.Create(1);
-
 
     // The below set of helpers will help us to put together the wifi NICs we want
     WifiHelper wifi;
@@ -165,7 +143,6 @@ int main(int argc, char *argv[]) {
     NetDeviceContainer devices = wifi.Install(wifiPhy, wifiMac, c);
     NetDeviceContainer fdevices = wifi.Install(wifiPhy, wifiMac, f);
 
-
     MobilityHelper mobility;
     mobility.SetPositionAllocator("ns3::GridPositionAllocator",
             "MinX", DoubleValue(0.0),
@@ -179,17 +156,15 @@ int main(int argc, char *argv[]) {
 
     mobility.Install(c);
 
-
     MobilityHelper fmobility;
-    Ptr<ListPositionAllocator> positionAlloc = CreateObject <ListPositionAllocator>();
-    positionAlloc ->Add(Vector(0, 0, 0));
+    Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator>();
+    positionAlloc->Add(Vector(0, 0, 0));
     fmobility.SetPositionAllocator(positionAlloc);
 
     fmobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
     fmobility.Install(f);
 
     Ptr<ConstantPositionMobilityModel> cvMob = f.Get(0)->GetObject<ConstantPositionMobilityModel>();
-
 
     /** Energy Model **/
     BasicEnergySourceHelper basicSourceHelper;
@@ -199,7 +174,6 @@ int main(int argc, char *argv[]) {
     radioEnergyHelper.Set("TxCurrentA", DoubleValue(0.0174));
     DeviceEnergyModelContainer deviceModels = radioEnergyHelper.Install(devices, sources);
 
-
     // Enable OLSR
     OlsrHelper olsr;
     Ipv4StaticRoutingHelper staticRouting;
@@ -208,12 +182,10 @@ int main(int argc, char *argv[]) {
     list.Add(staticRouting, 0);
     list.Add(olsr, 10);
 
-
     InternetStackHelper internet;
     internet.SetRoutingHelper(list); // has effect on the next Install ()
     internet.Install(c);
     internet.Install(f);
-
 
     NS_LOG_INFO("Build Topology.");
     CsmaHelper csma;
@@ -223,14 +195,11 @@ int main(int argc, char *argv[]) {
     csma.Install(c);
     csma.Install(f);
 
-
     Ipv4AddressHelper ipv4;
     NS_LOG_INFO("Assign IP Addresses.");
     ipv4.SetBase("10.1.1.0", "255.255.255.0");
     Ipv4InterfaceContainer IpContainter = ipv4.Assign(devices);
     Ipv4InterfaceContainer fIpContainter = ipv4.Assign(fdevices);
-
-
 
     TypeId tid = TypeId::LookupByName("ns3::UdpSocketFactory");
 
@@ -240,7 +209,6 @@ int main(int argc, char *argv[]) {
     farmerRx->Bind(farmerRxIsoc);
     farmerRx->SetRecvCallback(MakeCallback(&FarmerRxCallback));
 
-
     Ptr<Socket> farmerBcArr[25];
 
     for (uint32_t i = 0; i < c.GetN(); i++) {
@@ -249,7 +217,6 @@ int main(int argc, char *argv[]) {
         sensorRxSocket->SetAllowBroadcast(true);
         sensorRxSocket->Bind(sensorRxIsoc);
 
-
         Ptr<Socket> sensorTxSocket = Socket::CreateSocket(c.Get(i), tid);
         InetSocketAddress sensorTxIsoc = InetSocketAddress(fIpContainter.GetAddress(0), 80);
         sensorTxSocket->SetAllowBroadcast(true);
@@ -257,43 +224,30 @@ int main(int argc, char *argv[]) {
 
         sensorRxSocket->SetRecvCallback(MakeBoundCallback(&SensorRxCallback, sensorTxSocket));
 
-
         farmerBcArr[i] = Socket::CreateSocket(f.Get(0), tid);
         InetSocketAddress farmerBcIsoc = InetSocketAddress(IpContainter.GetAddress(i), 80);
         farmerBcArr[i]->Connect(farmerBcIsoc);
-
     }
     Simulator::Schedule(Seconds(30.0), &ManWalking, cvMob, farmerBcArr);
 
     for (int i = 0; i < 25; i++) {
-        Ptr<BasicEnergySource> basicSourcePtr = DynamicCast<BasicEnergySource> (sources.Get(i));
+        Ptr<BasicEnergySource> basicSourcePtr = DynamicCast<BasicEnergySource>(sources.Get(i));
         basicSourcePtr->TraceConnectWithoutContext("RemainingEnergy", MakeBoundCallback(&RemainingEnergy, i));
     }
 
-
     // Output what we are doing
-    NS_LOG_UNCOND("Testing from node " << " with grid distance " << GRID_DISTANCE);
-
-
+    NS_LOG_UNCOND("Testing from node "
+            << " with grid distance " << GRID_DISTANCE);
 
     Simulator::Stop(Seconds(300.0));
-
-
-
 
     AnimationInterface anim("wireless-animation.xml"); // Mandatory
     for (uint32_t i = 0; i < c.GetN(); ++i) {
         std::string str = "STA";
         str += '1' + i;
-        //std::stringstream tmp;
-        //std::string energyLevel;
-        //tmp << "Energy: " << anim.GetNodeEnergyFraction(c.Get (i));
-        //energyLevel = tmp.str();
-        //anim.UpdateNodeDescription (c.Get (i), energyLevel); // Optional
-        //anim.GetNodeEnergyFraction(c.Get (i));
+
         anim.UpdateNodeColor(c.Get(i), 255, 0, 0); // Optional
         anim.UpdateNodeSize(c.Get(i)->GetId(), 3.0, 3.0);
-
     }
     anim.UpdateNodeDescription(f.Get(0), "FARMER");
     anim.UpdateNodeSize(f.Get(0)->GetId(), 15.0, 15.0);
@@ -309,60 +263,31 @@ int main(int argc, char *argv[]) {
     Simulator::Run();
 
     monitor->CheckForLostPackets();
-    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier> (flowmon.GetClassifier());
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
     FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
     //    int j = 0;
     int lostPackets = 0;
     int rxPackets = 0;
     for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator i = stats.begin(); i != stats.end(); ++i) {
-        //        double usefulData = ((double) (i->second.rxBytes) / (double) (i->second.txBytes))*100;
-        //        double throughput = (double) i->second.rxBytes * 8.0 / (double) (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds()) / 1024; //kbps
-        double packetLossRate = ((double) i->second.lostPackets / (double) (i->second.lostPackets + i->second.rxPackets))*100;
-        //        double meanDelay = (i->second.delaySum.GetMilliSeconds() / (double) i->second.rxPackets);
         lostPackets += i->second.lostPackets;
         rxPackets += i->second.rxPackets;
-
-        // std::cout << packetLossRate << "\t";
-        //        std::cout << meanDelay << "\t";
-
-        //        std::cout << throughput << "\t";
-        // std::cout << usefulData << "\t";
-
-        //        std::cout << i->second.lostPackets << "\t";
-        //        std::cout << i->second.jitterSum << "\t";
-        //        std::cout << i->second.rxBytes << "\t";
-
-        //        std::cout << i->second.txBytes << "\t";
-        // std::cout << "\n";
-
-
-        data2.Add(i->second.rxBytes, packetLossRate);
-
-
-        data2.Add((double) i->second.jitterSum.GetSeconds(), (double) i->second.lostPackets);
-
     }
 
-    double avgLoss = ((double) lostPackets / (double) (lostPackets + rxPackets))*100;
+    double avgLoss = ((double) lostPackets / (double) (lostPackets + rxPackets)) * 100;
 
-    std::cout << PACKET_SIZE << "B - " << GRID_DISTANCE << "M - " << avgLoss << "% loss\n";
+    std::cout << "\n"
+            << PACKET_SIZE << "B - " << GRID_DISTANCE << "M - " << avgLoss << "% loss\n";
 
     Simulator::Destroy();
 
     for (int i = 0; i < 25; i++) {
         graf.AddDataset(data[i]);
-
     }
     std::ofstream plotFile("graf.plt");
     graf.GenerateOutput(plotFile);
     plotFile.close();
-    if (system("gnuplot graf.plt"));
+    if (system("gnuplot graf.plt"))
+        ;
 
-
-    graf2.AddDataset(data2);
-    std::ofstream plotFile2("graf.plt");
-    graf2.GenerateOutput(plotFile2);
-    plotFile2.close();
-    if (system("gnuplot graf.plt"));
     return 0;
 }
